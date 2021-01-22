507eef31a04427191acce3dc95d35ece02029bba
dynnah <dynnah_hanna@hotmail.com>
2021-01-22 14:33:42 -0300

    footer

---
+++ src/components/components/Footer/index.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import Facebook from '../../../assets/Facebook.png'
+import Instagram from '../../../assets/Instagram.png'
+import Twitter from '../../../assets/Twitter.png'
+import './index.css'
+
+function Footer() {
+  return (
+      <div class="footer">
+         <div className="footer-infos">
+             <p className="texto-denuncia"><span>Caso você tenha sofrido algum tipo de violência, faça uma denúncia!</span><span>Clique em um dos botões ao lado para ser redirecionada.</span></p>
+             <div className="info-btn">
+             <button className="delegacia" type="button" href="https://www.google.com">Delegacia Virtual</button>
+             <button className="disk-180">Ligue 180</button>
+             </div>
+             
+         </div>
+         <div className="footer-share">
+             <p>compartilhe o projeto</p>
+             <a href="http://google.com/"><button className="icons"><img className="icones" src={Facebook} alt="Facebook Logo"></img></button></a>
+             <a href="http://google.com/"><button className="icons"><img className="icones" src={Instagram} alt="Instagram Logo"></img></button></a>
+             <a href="http://google.com/"><button className="icons"><img className="icones" src={Twitter} alt="Twitter Logo"></img></button></a>
+             
+             <hr />
+
+             <p className="last-line">© Violência contra a mulher, Inc. 2021. </p>
+
+         </div>
+      </div>
+  );
+}
+
+export default Footer;
--- src/components/pages/Home/index.js
+++ src/components/pages/Home/index.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import Navbar from '../../components/Navbar'
+import Footer from '../../components/Footer'
 
 function Home() {
   return (
       <div>
           <Navbar />
+          <div>
+            <Footer />
+          </div>
       </div>
   );
 }
