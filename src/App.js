dde70cc44d5d0a6ea71fb68ae2d0761247458489
dynnah <dynnah_hanna@hotmail.com>
2021-01-23 18:59:26 -0300

    feat: :sparkles: custom scrollbar
    adding custom scrollbar to the app

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './App.css';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import { Scrollbars } from 'react-custom-scrollbars';
 import Home from './components/pages/Home'  
 import Navbar from './components/components/Navbar'
 import Footer from './components/components/Footer';
 
 function App() {
   return (
+    <Scrollbars style={{ width: '100%', minHeight: '100vh'}}>
     <Router>
       <Navbar />
       <Switch>
@@ -14,6 +16,7 @@ function App() {
       </Switch>
       <Footer/>
     </Router>
+    </Scrollbars>
   );
 }
 
