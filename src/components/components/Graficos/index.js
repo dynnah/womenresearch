2f56623a576d193ff76b445358f79bbddf1009bb
dynnah <dynnah_hanna@hotmail.com>
2021-01-28 15:21:58 -0300

    inital graphic component

---
+++ src/components/components/Graficos/index.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import './index.css'
+
+
+function Graficos() {
+  return (
+      <div className="graficos" id="graficos">
+          <div className="texto-inicial">
+            <h1 className="texto-principal">OS DADOS SÃO NÚMEROS</h1>
+            <p className="texto-secundario">e os números são nomes</p>
+            <p className="tagline">A violência contra a mulher é um grande problema social e as consequências podem ser duradouras</p>
+          </div>
+					<div className="brasil">
+						<h1>BRASIL</h1>
+						<hr />
+					</div>
+					<div className="caso-ano">
+						<p className="primeira-linha">Quantidade de casos por ano:</p>
+						<p className="segunda-linha">A cada 100 mil habitantes</p>
+						<hr />
+					</div>
+					<div className="tipos-violencia">
+						<p className="primeira-linha">Tipos de violência:</p>
+						<p className="primeira-linha">Ano:</p>
+						<hr />
+					</div>
+					<div className="faixa-etaria">
+						<p className="terceira-linha">Faixa etária mais afetada</p>
+						<hr />
+					</div>
+					<div className="etnia">
+						<p className="terceira-linha">Etnia mais afetada</p>
+						<hr />
+					</div>
+					<div className="faixa-e-etnia">
+						<p className="terceira-linha">Faixa Etária versus Etnia</p>
+						<hr />
+					</div>
+					<div className="autores">
+						<p className="terceira-linha">Quem comete esses crimes?</p>
+						<hr />
+					</div>
+      </div>
+  );
+}
+
+export default Graficos;
--- src/components/pages/Home/index.js
+++ src/components/pages/Home/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import Logo from '../../../assets/Logo.png'
-import Leis from '../../components/Leis';
 import Metodologia from '../../components/Metodologia';
+import Leis from '../../components/Leis';
+import Graficos from '../../components/Graficos'
 import chevronDown from '../../../assets/Desing.png'
 import triangleUp from '../../../assets/triangle-up.png'
 import './index.css'
@@ -11,19 +12,22 @@ function Home() {
   return (
     <div>
       <div>
-        <div className="home">
-          <div className="tagline">
-            <p className="tagline-text">ESCLARECENDO E EVIDENCIANDO DADOS SOBRE VIOLÊNCIA CONTRA A MULHER NO BRASIL</p>
-            <a className="chevron" href="#metodologia"><img className="chevron" src={chevronDown} alt="Arrow Down"></img></a>
-          </div>        
-          <div className="logo">
-            <img className="img-logo" src={Logo} alt="Logo"></img>
+        <div>
+          <div className="home">
+            <div className="tagline">
+              <p className="tagline-text">ESCLARECENDO E EVIDENCIANDO DADOS SOBRE VIOLÊNCIA CONTRA A MULHER NO BRASIL</p>
+              <a className="chevron" href="#metodologia"><img className="chevron" src={chevronDown} alt="Arrow Down"></img></a>
+            </div>        
+            <div className="logo">
+              <img className="img-logo" src={Logo} alt="Logo"></img>
+            </div>
+            <img className="triangle-up" src={triangleUp} alt="triangles"></img>
           </div>
-          <img className="triangle-up" src={triangleUp} alt="triangles"></img>
+          <Metodologia />
         </div>
-        <Metodologia />
+          <Leis />
       </div>
-        <Leis />
+      <Graficos />
     </div>  
   );
 }
